df212c0d39318398a9fb78b10519978e6a699762
zgf <1270782070@qq.com>
2018-05-03 14:15:33 +0800

    登录页面

---
+++ src/components/login/login.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="login">
+        <div class="login-i">
+            <h1 class="title">后台管理</h1>
+            <el-input v-model="name" placeholder="请输入用户名">
+            </el-input>
+            <el-input v-model="password" placeholder="请输入密码">
+            </el-input>
+            <el-button type="primary" :disabled="!name||!password" @click="login()">登录</el-button>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import crypto from 'crypto'
+    import {ERR_OK, SUCCESS_CODE, REEOR_CODE} from '../../common/js/config'
+    export default {
+        data() {
+            return {
+                name: '',
+                password: ''
+            }
+        },
+        methods: {
+            login() {
+                let md5 = crypto.createHash('md5')
+                let password = md5.update(this.password).digest('hex')
+                this.$ajax.get(`/login?name=${this.name}&password=${password}`).then((response) => {
+                    if (ERR_OK === response.status) {
+                        if (response.data.statueCode === SUCCESS_CODE) {
+
+                        } else if (response.data.statueCode === REEOR_CODE) {
+                            this.$message({
+                                message: response.data.msg,
+                                type: 'error',
+                                duration: 1000
+                            })
+                        }
+                    } else {
+                        this.$message({
+                            message: '登录失败',
+                            type: 'error',
+                            duration: 1000
+                        })
+                    }
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    .login {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .login-i {
+        width: 300px;
+        padding: 40px;
+        background: #57faff;
+        border-radius: 5px;
+    }
+
+    .login-i .title {
+        font-size: 20px;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    .login-i .el-input {
+        margin-bottom: 15px;
+    }
+
+    .login-i .el-button {
+        width: 100%;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,29 @@ import '@/common/css/style.css'
 Vue.use(Router)
 Vue.use(ElementUI)
 
-const routes = []
+const Login = (resolve) => {
+  import('@/components/login/login').then((module) => {
+    resolve(module)
+  })
+}
+const routes = [
+  {
+    path: '',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login,
+    meta: {title: '登录'}
+  }
+]
 const router = new Router({
   mode: 'history',
   linkActiveClass: 'active',
   routes: routes
 })
-
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title
+  next()
+})
 export default router
