87b6be5d377f30f7dee9884b8ff71c84ffdd8970
zgf <1270782070@qq.com>
2018-05-07 18:07:59 +0800

    验证码

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"//192.168.1.6:8888"'
+  BASE_URL: '"//192.168.1.5:8888"'
 })
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,84 +1,104 @@
 <template>
-    <div class="login">
-        <div class="login-i">
-            <h1 class="title">后台管理</h1>
-            <el-input v-model="name" placeholder="请输入用户名">
-            </el-input>
-            <el-input v-model="password" placeholder="请输入密码" type="password">
-            </el-input>
-            <el-button type="primary" :disabled="!name||!password" @click="login()">登录</el-button>
-        </div>
-    </div>
+  <div class="login">
+    <form name="login"  class="login-i">
+      <h1 class="title">后台管理</h1>
+      <el-input v-model="loginForm.name" placeholder="请输入用户名">
+      </el-input>
+      <el-input v-model="password" placeholder="请输入密码" type="password">
+      </el-input>
+      <div>
+        <el-input v-model="loginForm.captcha" placeholder="请输入验证码" class="captcha fl">
+        </el-input>
+        <div v-html="svgCaptcha" class="svgCaptcha fl cursor" @click="getCaptcha"></div>
+      </div>
+      <el-button type="primary" :disabled="!loginForm.name||!password||!loginForm.captcha" @click="login()">登录</el-button>
+    </form>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    import crypto from 'crypto'
-    import {ERR_OK, SUCCESS_CODE, ERROR_CODE} from '../../common/js/config'
-    export default {
-        data() {
-            return {
-                name: '',
-                password: ''
+  import crypto from 'crypto'
+  import { stringify } from 'qs'
+  import {ERR_OK, SUCCESS_CODE, ERROR_CODE} from '../../common/js/config'
+  export default {
+    data() {
+      return {
+        loginForm: {},
+        password: '',
+        svgCaptcha: ''
+      }
+    },
+    created() {
+      this.getCaptcha()
+    },
+    methods: {
+      login() {
+        let md5 = crypto.createHash('md5')
+        this.loginForm.password = md5.update(this.password).digest('hex')
+        this.$ajax.get(`/login?${stringify(this.loginForm)}`).then((response) => {
+          console.log(response)
+          if (ERR_OK === response.status) {
+            if (response.data.statueCode === SUCCESS_CODE) {
+              this.$router.push('/articleManage')
+            } else if (response.data.statueCode === ERROR_CODE) {
+              this.$message({
+                message: response.data.msg,
+                type: 'error',
+                duration: 1000
+              })
             }
-        },
-        methods: {
-            login() {
-                let md5 = crypto.createHash('md5')
-                let password = md5.update(this.password).digest('hex')
-                this.$ajax.get(`/login?name=${this.name}&password=${password}`).then((response) => {
-                    if (ERR_OK === response.status) {
-                        if (response.data.statueCode === SUCCESS_CODE) {
-                            this.$router.push('/articleManage')
-                        } else if (response.data.statueCode === ERROR_CODE) {
-                            this.$message({
-                                message: response.data.msg,
-                                type: 'error',
-                                duration: 1000
-                            })
-                        }
-                    } else {
-                        this.$message({
-                            message: '登录失败',
-                            type: 'error',
-                            duration: 1000
-                        })
-                    }
-                })
-            }
-        }
+          } else {
+            this.$message({
+              message: '登录失败',
+              type: 'error',
+              duration: 1000
+            })
+          }
+        })
+      },
+      getCaptcha() {
+        this.$ajax.get('/captcha').then((response) => {
+          if (ERR_OK === response.status) {
+            this.svgCaptcha = response.data
+          }
+        })
+      }
     }
+  }
 </script>
 
 <style>
-    .login {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+  .login {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-    .login-i {
-        width: 300px;
-        padding: 40px;
-        background: rgba(87, 250, 255, .8);
-        border-radius: 5px;
-    }
+  .login-i {
+    width: 300px;
+    padding: 40px;
+    background: rgba(87, 250, 255, .8);
+    border-radius: 5px;
+  }
 
-    .login-i .title {
-        font-size: 20px;
-        text-align: center;
-        margin-bottom: 20px;
-    }
+  .login-i .title {
+    font-size: 20px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
 
-    .login-i .el-input {
-        margin-bottom: 15px;
-    }
-
-    .login-i .el-button {
-        width: 100%;
-    }
+  .login-i .el-input {
+    margin-bottom: 15px;
+  }
+  .login-i .captcha.el-input,.svgCaptcha{
+    width: 50%;
+  }
+  .login-i .el-button {
+    width: 100%;
+  }
 </style>
